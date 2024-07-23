599d4bc5cdfee8f4e6fdbd0e7ec693994aaefc09
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-07-23 12:42:47 +0300

    final classworc

---
+++ main.py
@@ -0,0 +1,71 @@
+# #Часть первая
+# import requests
+# from bs4 import BeautifulSoup
+#
+# url = "http://quotes.toscrape.com/"
+# response = requests.get(url)
+# html = response.text
+#
+# soup = BeautifulSoup(html, "html.parser")
+#
+# links = soup.find_all("a")
+# for link in links:
+#     print(link.get("href"))
+
+#Часть вторая
+import requests
+from bs4 import BeautifulSoup
+
+
+# Создаём функцию, которая будет получать информацию
+def get_english_words():
+    url = "https://randomword.com/"
+    try:
+        response = requests.get(url)
+
+        # Создаём объект Soup
+        soup = BeautifulSoup(response.content, "html.parser")
+        # Получаем слово. text.strip удаляет все пробелы из результата
+        english_word = soup.find("div", id="random_word").text.strip()
+        # Получаем описание слова
+        word_definition = soup.find("div", id="random_word_definition").text.strip()
+        # Чтобы программа возвращала словарь
+        return {
+            "english_word": english_word,
+            "word_definition": word_definition
+        }
+    # Функция, которая сообщит об ошибке, но не остановит программу
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return None
+
+
+# Создаём функцию, которая будет делать саму игру
+def word_game():
+    print("Добро пожаловать в игру")
+    while True:
+        # Создаём функцию, чтобы использовать результат функции-словаря
+        word_dict = get_english_words()
+        if not word_dict:
+            print("Ошибка при получении слова. Попробуйте позже.")
+            break
+
+        word = word_dict.get("english_word")
+        word_definition = word_dict.get("word_definition")
+
+        # Начинаем игру
+        print(f"Значение слова - {word_definition}")
+        user = input("Что это за слово? ")
+        if user.lower() == word.lower():
+            print("Все верно!")
+        else:
+            print(f"Ответ неверный, было загадано это слово - {word}")
+
+        # Создаём возможность закончить игру
+        play_again = input("Хотите сыграть еще раз? y/n: ")
+        if play_again.lower() != "y":
+            print("Спасибо за игру!")
+            break
+
+
+word_game()
